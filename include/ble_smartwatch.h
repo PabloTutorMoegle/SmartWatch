a04e50dec730088520a0970ce75d29b050944832
PabloTutorMoegle <pablotutormoegle@gmail.com>
2026-05-20 13:27:16 +0200

    Bluetooth updates

---
+++ include/ble_smartwatch.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+#include <functional>
+
+class SmartWatchBLE {
+public:
+    void begin();
+    void sendIMU(int16_t ax, int16_t ay, int16_t az, int16_t gx, int16_t gy, int16_t gz);
+    void sendTemp(float celsius);
+    void sendButton(const char* state);
+    bool connected();
+    void onCommand(std::function<void(uint8_t)> cb);
+
+private:
+    BLEServer* server = nullptr;
+    BLEService* service = nullptr;
+    BLECharacteristic* charIMU = nullptr;
+    BLECharacteristic* charTemp = nullptr;
+    BLECharacteristic* charButton = nullptr;
+    BLECharacteristic* charCommand = nullptr;
+    bool deviceConnected = false;
+    std::function<void(uint8_t)> cmdCallback;
+
+    class ServerHandler : public BLEServerCallbacks {
+    public:
+        SmartWatchBLE* parent;
+        void onConnect(BLEServer* s) override;
+        void onDisconnect(BLEServer* s) override;
+    };
+
+    class CommandHandler : public BLECharacteristicCallbacks {
+    public:
+        SmartWatchBLE* parent;
+        void onWrite(BLECharacteristic* c) override;
+    };
+};
